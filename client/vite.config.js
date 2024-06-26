0c4e9ada310da64e066a02a59d44f2facc639aee
Prasana Kumar M D <prasana.kumar.md@oracle.com>
2024-06-26 11:58:50 +0530

    commit in vite.config.js for no open ports detected issue

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,11 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    strictPort: true,
+    port: 8000,
+  },
   build: {
     rollupOptions: {
       output: {
